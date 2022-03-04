584431b6c0693436244205059e8adbe3c523b10f
vzarate26 <vzarate26@gmail.com>
2022-03-03 22:48:10 -0500

    flesh out Nav bar component and css

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+
+function Nav() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul className='nav-links'>
+        <li>Oliver</li>
+        <li>Oscar</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
